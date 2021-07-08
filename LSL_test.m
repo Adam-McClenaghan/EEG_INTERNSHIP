3d3db455b808efbcefeb7b4fe3f3b255d168592d
Adam-McClenaghan <81857960+Adam-McClenaghan@users.noreply.github.com>
2021-07-08 14:41:57 +0100

    LSL changes added2

--- LSL_test.m
+++ LSL_test.m
@@ -1,7 +1,43 @@
 %% instantiate the library
+fs = 125;
+trial_length = 5;
+stimfreq = [8,9,10,11,12,13,14,15];
+roi = [1,2,7,8,11,12,15,16]
+num_chan = 16;
+Data_arr = zeros(fs*trial_length, num_chan);
+CCA_data = zeros(fs*trial_length, length(roi));
+uVscale = (4500000 / 24 / (2^23 -1));
+
+%% Create comparisson matrix
+
+S = fs;
+T = size(Data_arr, 1);
+t = (1/S:1/S:T/S);
+num_harmonics = 4;
+num_inputs = length(stimfreq);
+
+compmat = zeros(2*num_harmonics,length(t),num_inputs);
+
+for i = 1:length(compmat(1,1,:))
+
+
+input_freq = stimfreq(i);
+ 
+    for k = 1:2:2*num_harmonics
+    compmat(k,:,i) = sin((k+1)*pi*input_freq*t);
+    end
+
+    for j = 2:2:2*num_harmonics
+    compmat(j,:,i) = cos(j*pi*input_freq*t);    
+    end
+
+end
+
+%% Get LSL data
+
 disp('Loading the library...');
 lib = lsl_loadlib(); 
-version = lsl_library_version(lib)
+version = lsl_library_version(lib);
 
 % resolve a stream...
 disp('Resolving an EEG stream...');
@@ -15,9 +51,17 @@ inlet = lsl_inlet(result{1});
 
 disp('Now receiving data...');
 while true
-    % get data from the inlet
-    [vec,ts] = inlet.pull_sample();
-    % and display it
-    fprintf('%.2f\t',vec);
-    fprintf('%.5f\n',ts);
+    count = 1;
+    while count <= size(Data_arr, 1)
+        % get data from the inlet
+        [vec,ts] = inlet.pull_sample();
+        % and display it
+        Data_arr(count,:) = vec.*uVscale;
+        
+        count = count + 1;
+       
+%         fprintf('%.2f\t',vec);
+%         fprintf('%.5f\n',ts);
+    end
+    CCA_live
 end
